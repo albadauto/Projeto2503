9bf27c62aa0eebc3b1dda5193b2a51361446a362
albadauto <joseadauto923@gmail.com>
2021-11-23 11:48:39 -0300

    FIRST COMMIT

--- app.js
+++ app.js
@@ -3,13 +3,14 @@ const app = express()
 require('dotenv').config()
 const port = process.env.SERVER_PORT || 8081
 const mongoose = require('mongoose')
+const index = require('./routes/index')
 //CONFIG
 app.set('view engine', 'ejs')
 app.use(express.urlencoded({extended:false}))
 app.use(express.static(__dirname + 'public'))
 //ROUTES
-
+app.use('/', index)
 //MONGODB
 mongoose.connect(`${process.env.MONGOURI}`, () => console.log('Conectado ao banco de dados com sucesso!'))
 //SERVER
-app.listen(port, () => console.log(`PROJETO ${process.env.PROJECTNAME} rodando na porta ${port}` ))
+app.listen(port, () => console.log(`PROJETO ${process.env.PROJECTNAME} rodando na porta ${port}`))
